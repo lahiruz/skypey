9deffa7663fcfe3a7ceaf24f5e33427f8a337eed
Lahiru Jayamanna <lsampath999@gmail.com>
2019-05-12 17:33:38 +0530

    Add base components

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './Main.css';
+
+class Main extends Component {
+	render(){
+		return (
+			<main className="Main">Main</main>
+		)
+	}
+}
+
+export default Main;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './Sidebar.css';
+
+class Sidebar extends Component {
+	render(){
+		return (
+			<aside className="Sidebar">Sidebar</aside>
+		)
+	}
+}
+
+export default Sidebar;
